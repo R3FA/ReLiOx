c8170536ee3487498f0bfc1c3f86ad446d4e4a64
R3FA <faris.skopak@protonmail.com>
2024-12-19 17:41:45 +0100

    Migration for creation database tables - populate data inside tables- create model data

---
+++ Backend/migration.py
@@ -0,0 +1,19 @@
+from main import app
+from database_models import db, DailyObligationsModel
+from models import DailyObligation
+
+
+with app.app_context():
+    db.create_all()
+    print("Tables have been created successfully.")
+
+    # Populating daily_obligations table with default data.
+    if db.session.query(DailyObligationsModel).count() == 0:
+        for obligation in DailyObligation:
+            db.session.add(DailyObligationsModel(
+                daily_obligation_type=obligation.name))
+        db.session.commit()
+        print(
+            "'daily_obligations' table has been populated with default daily obligations.")
+    else:
+        print("'daily_obligations' table has already been populated with default data.")
