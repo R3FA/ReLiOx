32f3d5c2863c1d4adedf843b3ec1ba434208bed6
R3FA <faris.skopak@protonmail.com>
2024-12-24 20:03:39 +0100

    Create navigation bar and side bar with user cards

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'reliox-app';
+  public navBarTitle: string = 'ReLiOx - Gaming Session Manager';
+  public sideBarUserPanelTitle: string = 'User Panel';
 }
