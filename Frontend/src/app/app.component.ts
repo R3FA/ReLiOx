6a16da40aa6928bba2708960d055054ea282d3ec
Faris Skopak <77499895+R3FA@users.noreply.github.com>
2024-12-25 16:03:32 +0100

    Create Base UI (#9)
    * Import bootstrap in angular project
    
    * Import fontawesome icons in angular project
    
    * Create navigation bar and side bar with user cards
    
    * Create modal form for user creation
    
    * Finish Base UI
    
    * Implement some missing UI features

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [CommonModule],
 })
 export class AppComponent {
-  title = 'reliox-app';
+  public navBarTitle: string = 'ReLiOx - Gaming Session Manager';
+  public sideBarUserPanelTitle: string = 'User Panel';
+  public ifUserNotChosen: boolean = false;
 }
