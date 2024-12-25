2daa154b6ba5b7fc32ac69fdaa900191b52a2d76
R3FA <faris.skopak@protonmail.com>
2024-12-25 15:56:08 +0100

    Finish Base UI

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  imports: [CommonModule],
 })
 export class AppComponent {
   public navBarTitle: string = 'ReLiOx - Gaming Session Manager';
   public sideBarUserPanelTitle: string = 'User Panel';
+  public ifUserNotChosen: boolean = true;
 }
