8e7430ab50414f34c843e1e7a824858496bed2e3
R3FA <faris.skopak@protonmail.com>
2024-12-31 14:12:25 +0100

    Implement checks to ensure that users can generate the 300k dataset only once and insert it into the table.

--- Backend/migration.py
+++ Backend/migration.py
@@ -48,54 +48,58 @@ def calculate_session_duration(fatigue_level, stress_level, daily_obligations):
 def generate_dataset(num_samples):
     data: list[AgentData] = []
 
-    print("Initiating the generation of 300,000 datasets for the agent. Please wait!")
+    if db.session.query(AgentTrainedDataModel).count() != 0:
+        print(
+            "The 'agent_trained_data' table is already populated with the agent's training data.")
+        return []
+    else:
+        print("Initiating the generation of 300,000 datasets for the agent. Please wait!")
 
-    for _ in range(num_samples):
-        fatigue = random.choice(FatigueLevel.list())
-        stress = random.choice(StressLevel.list())
-        daily_obligations = generate_daily_obligations_for_dataset()
-        session_duration = calculate_session_duration(
-            fatigue, stress, daily_obligations)
+        for _ in range(num_samples):
+            fatigue = random.choice(FatigueLevel.list())
+            stress = random.choice(StressLevel.list())
+            daily_obligations = generate_daily_obligations_for_dataset()
+            session_duration = calculate_session_duration(
+                fatigue, stress, daily_obligations)
 
-        data.append(AgentData(fatigue, stress, sum(
-            daily_obligations), session_duration))
+            data.append(AgentData(fatigue, stress, sum(
+                daily_obligations), session_duration))
 
-    if db.session.query(AgentTrainedDataModel).count() == 0:
         for data_set in data:
             db.session.add(AgentTrainedDataModel(data_set.fatigue, data_set.stress,
-                           data_set.daily_obligations, data_set.session_duration))
+                                                 data_set.daily_obligations, data_set.session_duration))
         db.session.commit()
         print(
             "The 'agent_trained_data' table has been populated with the generated training data.")
-    else:
-        print(
-            "The 'agent_trained_data' table is already populated with the agent's training data.")
 
-    return data
+        return data
 
 
 def train_model(data: list[AgentData]):
-    print('Training the agent on 300,000 datasets has begun. Please wait!')
+    if len(data) != 0:
+        print('Training the agent on 300,000 datasets has begun. Please wait!')
 
-    model = MLPRegressor(hidden_layer_sizes=(
-        10,), max_iter=500, random_state=42)
-    X = []
-    Y = []
+        model = MLPRegressor(hidden_layer_sizes=(
+            10,), max_iter=500, random_state=42)
+        X = []
+        Y = []
 
-    for data_set in data:
-        X.append([data_set.fatigue, data_set.stress,
-                 data_set.daily_obligations])
-        Y.append(data_set.session_duration)
+        for data_set in data:
+            X.append([data_set.fatigue, data_set.stress,
+                      data_set.daily_obligations])
+            Y.append(data_set.session_duration)
 
-    X_train, X_test, Y_train, Y_test = train_test_split(
-        X, Y, test_size=0.2, random_state=42)
+        X_train, X_test, Y_train, Y_test = train_test_split(
+            X, Y, test_size=0.2, random_state=42)
 
-    model.fit(X_train, Y_train)
+        model.fit(X_train, Y_train)
 
-    with open('trained-model.sav', 'wb') as F:
-        pickle.dump(model, F)
+        with open('trained-model.sav', 'wb') as F:
+            pickle.dump(model, F)
 
-    print("The agent has been successfully trained and saved to the file: trained-model.sav")
+        print("The agent has been successfully trained and saved to the file: trained-model.sav")
+    else:
+        print("Agent training is not required as the dataset is already populated and the agent has been previously trained.")
 
 
 with app.app_context():
